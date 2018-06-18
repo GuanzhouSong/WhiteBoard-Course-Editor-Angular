878167187c64c2f411c2f232c236ceb4f079aff5
Ishan Patel <i@patelishan.com>
2018-06-18 06:06:16 -0400

    User login validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Route, Router} from "@angular/router";
 import {UserServiceClient} from "../services/user.service.client";
+import swal from "sweetalert";
 
 @Component({
   selector: 'app-login',
@@ -11,17 +12,26 @@ export class LoginComponent implements OnInit {
 
   username;
   password;
+
   login(username, password) {
     console.log([username, password]);
     this.service
       .login(username, password)
-      .then(() => {
-        this.router.navigate(['profile']);
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {
+        if (myJson.success === false) {
+          swal("Error!", myJson.message, "error");
+        } else {
+          this.router.navigate(['profile']);
+        }
       });
   }
 
   constructor(private router: Router,
-              private service: UserServiceClient) { }
+              private service: UserServiceClient) {
+  }
 
   ngOnInit() {
   }
